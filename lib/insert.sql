8ac94108dcc6433b66386cfa9a4ed1e232a44232
Scarlett Eller <rq6715@gmail.com>
2022-02-24 17:25:00 -0500

    commit tree f261fa6614bee6d1593c458dbebc56fcff3c62a6

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO 
+    bears
+VALUES(1, 'Mr. Chocolate', 56, "M", "brown", "calm", 1);
+
+INSERT INTO 
+    bears
+VALUES(2, 'Rowdy', 34, "M", "brown", "calm", 0);
+
+INSERT INTO 
+    bears
+VALUES(3, 'Tabitha', 36, "F", "brown", "calm", 1);
+
+INSERT INTO 
+    bears
+VALUES(4, 'Sergeant Brown', 76, "M", "brown", "calm", 0);
+
+INSERT INTO 
+    bears
+VALUES(5, 'Melissa', 24, "F", "brown", "calm", 1);
+
+INSERT INTO 
+    bears
+VALUES(6, 'Grinch', 60, "M", "brown", "calm", 1);
+
+INSERT INTO 
+    bears
+VALUES(7, 'Wendy', 56, "F", "brown", "calm", 1);
+
+INSERT INTO 
+    bears
+VALUES(8, null, 56, "M", "brown", "calm", 1);
+
